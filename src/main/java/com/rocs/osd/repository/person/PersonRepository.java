957458d8adee2c669ce66c6dbb6ff8b4a47e59ab
zhairanicolepacheco <zhaira.nicole.a.pacheco@gmail.com>
2024-05-16 16:49:57 +0800

    Refactored Domain

--- src/main/java/com/rocs/osd/repository/person/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rocs.osd.repository.person;
-
-import com.rocs.osd.domain.person.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-}
